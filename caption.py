85d51bba5456f25c80176f77af7f04d2faa64833
kianakaslana648 <90156516+kianakaslana648@users.noreply.github.com>
2023-04-26 15:41:53 -0400

    Add files via upload

--- caption.py
+++ caption.py
@@ -4,5 +4,5 @@ import PIL
 
 def image_captioning(raw_image: PIL.Image.Image):
   inputs = share.shared_items.caption_processor(raw_image, return_tensors="pt")
-  out = share.shared_items.caption_model.generate(**inputs)
+  out = share.shared_items.caption_model.generate(**inputs, max_length=200)
   return share.shared_items.caption_processor.decode(out[0], skip_special_tokens=True, max_new_tokens=200)
